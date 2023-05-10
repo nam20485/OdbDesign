3e5daa8a505af36c4765870dfb35a82faa47d47d
Nathan Miller <nmiller217@gmail.com>
2023-05-10 16:51:13 -0700

    add initial project

---
+++ OdbDesignApp/OdbDesignApp.cpp
@@ -0,0 +1,12 @@
+﻿// OdbDesignApp.cpp : Defines the entry point for the application.
+//
+
+#include "OdbDesignApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
