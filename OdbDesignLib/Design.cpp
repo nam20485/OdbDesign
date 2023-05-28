4fa4cb398296e529f8550f7939be1124abb084b3
Nathan Miller <nmiller217@gmail.com>
2023-05-28 13:45:05 -0700

    add Build([...]) methods

--- OdbDesignLib/Design.cpp
+++ OdbDesignLib/Design.cpp
@@ -1,1 +1,48 @@
 #include "Design.h"
+
+namespace OdbDesign::Lib::Design
+{	
+	Design::Design()
+	{
+	}
+
+	Design::~Design()
+	{
+	}
+
+	bool Design::Build(std::string designDirectory)
+	{
+		auto pFileModel = std::make_shared<FileModel::FileModel>(designDirectory);
+		if (pFileModel->ParseFileModel())
+		{
+			return Build(pFileModel);
+		}
+		return false;
+	}
+
+	bool Design::Build(std::shared_ptr<FileModel::FileModel> pFileModel)
+	{
+		m_pFileModel = pFileModel;
+		return Build();		
+	}
+
+	bool Design::Build()
+	{
+		if (m_pFileModel == nullptr) return false;
+
+		BuildNets();
+
+		return true;
+	}
+
+	bool Design::BuildComponents()
+	{
+		return false;
+	}
+
+	bool Design::BuildNets()
+	{
+		return false;
+	}
+
+} // namespace OdbDesign::Lib::Design
--- OdbDesignLib/Design.h
+++ OdbDesignLib/Design.h
@@ -2,6 +2,7 @@
 
 #include "export.h"
 #include <string>
+#include <memory>
 #include "Net.h"
 #include "Component.h"
 #include "FileModel.h"
@@ -11,19 +12,27 @@ namespace OdbDesign::Lib::Design
 {
 	class DECLSPEC Design
 	{
-	public:
-		Design(const OdbDesign::Lib::FileModel::FileModel& fileModel);
+	public:	
+		Design();
 		~Design();
+
+		bool Build(std::string designDirectory);
+		bool Build(std::shared_ptr<FileModel::FileModel> pFileModel);
 
 	private:
 		std::string m_productModel;
 		std::string m_name;
 
+		std::string m_designDirectory;		
+		std::shared_ptr<FileModel::FileModel> m_pFileModel;		
+
 		Net::StringMap m_netsByName;
 		Component::StringMap m_componentsByName;
 		Via::StringMap m_viasByName;
 
-		bool BuildFromFileModel();
+		bool Build();
+		bool BuildComponents();
+		bool BuildNets();
 
 	};
 }
