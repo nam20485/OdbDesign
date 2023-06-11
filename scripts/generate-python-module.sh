8a36e6fee24e0d750c3727ece488fce229ce4bfa
Nathan <nmiller217@gmail.com>
2023-06-11 11:49:43 -0700

    add OS-specific swig scripts

--- scripts/generate-python-module.sh
+++ scripts/generate-python-module.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-swig -c++ -outdir ./PyOdbDesignLib -python ./OdbDesignLib/OdbDesignLib.i
+#swig -c++ -outdir ./PyOdbDesignLib -python ./OdbDesignLib/OdbDesignLib.i
+# "include" the .cmd file by source executing here, so we don't have to duplicate the commands
+source scripts/generate-python-module.cmd
