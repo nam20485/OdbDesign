68a8f8e9156baec52aaf9686602bca8a96c7f63b
Nathan Miller <nmiller217@gmail.com>
2023-10-04 11:33:11 -0700

    change ouput

--- OdbDesignApp/OdbDesignApp.cpp
+++ OdbDesignApp/OdbDesignApp.cpp
@@ -4,6 +4,9 @@
 #include "FileArchive.h"
 #include "macros.h"
 
+bool TestRigidFlexDesign();
+bool TestSampleDesign();
+
 
 int main()
 {
@@ -11,6 +14,51 @@ int main()
 
     //OdbDesign::Lib::helloLib();
 
+    auto success = TestRigidFlexDesign();
+    if (!success)
+    {
+        return 1;
+    }
+
+    std::cout << "success!" << std::endl;
+
+    success = TestSampleDesign();
+    if (!success)
+    {
+        return 1;
+    }
+
+    std::cout << "success!" << std::endl;    
+
+    return 0;
+}
+
+bool TestSampleDesign()
+{
+    std::string sampleDesignPath;
+    if (Odb::Lib::IsMsvc())
+    {
+        sampleDesignPath = R"(C:\Users\nmill\OneDrive\Documents\ODB++\Samples\sample_design)";
+    }
+    else
+    {
+        sampleDesignPath = R"(/mnt/c/Users/nmill/OneDrive/Documents/ODB++/Samples/sample_design)";
+    }
+
+    std::cout << "Parsing " << sampleDesignPath << "... ";
+
+    Odb::Lib::FileModel::Design::FileArchive sampleOdbDesign(sampleDesignPath);
+    auto success = sampleOdbDesign.ParseFileModel();
+    if (! success)
+    {     
+        return false;
+    }
+
+    return true;
+}
+
+bool TestRigidFlexDesign()
+{   
     std::string rigidFlexDesignPath;
     if (Odb::Lib::IsMsvc())
     {
@@ -18,14 +66,16 @@ int main()
     }
     else
     {
-        rigidFlexDesignPath = R"(/mnt/c/Users/nmill/OneDrive/Documents/ODB++/Samples/designodb_rigidflex)";		
+        rigidFlexDesignPath = R"(/mnt/c/Users/nmill/OneDrive/Documents/ODB++/Samples/designodb_rigidflex)";
     }
+
+    std::cout << "Parsing " << rigidFlexDesignPath << "... ";
 
     Odb::Lib::FileModel::Design::FileArchive rigidFlexOdbDesign(rigidFlexDesignPath);
     auto success = rigidFlexOdbDesign.ParseFileModel();
     if (!success)
     {
-        return 1;
+        return false;
     }
 
     const auto& findStep = rigidFlexOdbDesign.GetStepsByName().find("cellular_flip-phone");
@@ -47,39 +97,39 @@ int main()
                 auto subnetType = pSubnetRecord->type;
                 if (subnetType == Odb::Lib::FileModel::Design::EdaDataFile::NetRecord::SubnetRecord::Type::Toeprint)
                 {
-					auto pViaSubnetRecord = std::dynamic_pointer_cast<Odb::Lib::FileModel::Design::EdaDataFile::NetRecord::ToeprintSubnetRecord>(pSubnetRecord);
-					auto viaType = pViaSubnetRecord->type;
+                    auto pViaSubnetRecord = std::dynamic_pointer_cast<Odb::Lib::FileModel::Design::EdaDataFile::NetRecord::ToeprintSubnetRecord>(pSubnetRecord);
+                    auto viaType = pViaSubnetRecord->type;
                     if (viaType == Odb::Lib::FileModel::Design::EdaDataFile::NetRecord::ToeprintSubnetRecord::Type::Via)
                     {
 
                     }
-				}               
+                }
             }
         }
 
         auto& packageRecords = edaData.GetPackageRecords();
         if (packageRecords.size() > 0)
         {
-			auto& pPackageRecord = packageRecords[0];
-			auto& packageName = pPackageRecord->name;
-		}
-        
+            auto& pPackageRecord = packageRecords[0];
+            auto packageName = pPackageRecord->name;
+        }
+
         // layers
         auto& layersByName = pStep->GetLayersByName();
         auto layerFind = layersByName.find(Odb::Lib::FileModel::Design::LayerDirectory::TOP_COMPONENTS_LAYER_NAME);
         if (layerFind != layersByName.end())
         {
-			auto& pLayer = layerFind->second;
-			auto layerName = pLayer->GetName();			
-		}
-        
+            auto& pLayer = layerFind->second;
+            auto layerName = pLayer->GetName();
+        }
+
         // netlist
         const auto& netlistsByName = pStep->GetNetlistsByName();
         auto netlistFind = netlistsByName.find("cadnet");
         if (netlistFind != netlistsByName.end())
         {
-			auto& pNetlist = netlistFind->second;
-			auto netlistName = pNetlist->GetName();
+            auto& pNetlist = netlistFind->second;
+            auto netlistName = pNetlist->GetName();
             auto& netNames = pNetlist->GetNetNames();
             for (auto& netName : netNames)
             {
@@ -87,28 +137,9 @@ int main()
                 {
 
                 }
-			}
-		}        
+            }
+        }
     }
 
-    std::string sampleDesignPath;
-    if (Odb::Lib::IsMsvc())
-    {
-		sampleDesignPath = R"(C:\Users\nmill\OneDrive\Documents\ODB++\Samples\sample_design)";
-	}
-    else
-    {
-		sampleDesignPath = R"(/mnt/c/Users/nmill/OneDrive/Documents/ODB++/Samples/sample_design)";
-	}
-
-    Odb::Lib::FileModel::Design::FileArchive sampleOdbDesign(sampleDesignPath);
-    success = sampleOdbDesign.ParseFileModel();
-    if (!success)
-    {
-        return 1;
-    }
-
-    std::cout << "success" << std::endl;
-
-    return 0;
+    return true;
 }
