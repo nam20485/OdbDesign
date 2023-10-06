c6a6476d9d4311ea156c20d04ab40e12c54fbc52
Nathan Miller <nmiller217@gmail.com>
2023-10-06 14:29:11 -0700

    add basic Crow server serving hello world html

--- OdbDesignServer/OdbDesignServer.cpp
+++ OdbDesignServer/OdbDesignServer.cpp
@@ -3,8 +3,22 @@
 
 #include "OdbDesignServer.h"
 #include "OdbDesign.h"
+#include "crow.h"
 
 int main()
 {
+	crow::SimpleApp app;
+
+	app.loglevel(crow::LogLevel::Info);
+
+	CROW_ROUTE(app, "/")([]() {
+		//return "Hello world";
+		auto page = crow::mustache::load_text("helloworld.html");
+		return page;
+		});
+
+
+	app.port(18080).multithreaded().run();
+
 	return 0;
 }
