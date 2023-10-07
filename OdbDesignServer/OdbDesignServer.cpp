0c73160cb6c94df17fccb360ef9eedf09b148a95
Nathan Miller <nmiller217@gmail.com>
2023-10-07 10:25:01 -0700

    refactor main

--- OdbDesignServer/OdbDesignServer.cpp
+++ OdbDesignServer/OdbDesignServer.cpp
@@ -4,21 +4,47 @@
 #include "OdbDesignServer.h"
 #include "OdbDesign.h"
 #include "crow.h"
+#include "Controllers/HelloWorldController.h"
+
+
+void DefineRoutes(crow::SimpleApp& app);
 
 int main()
 {
 	crow::SimpleApp app;
 
-	app.loglevel(crow::LogLevel::Info);
+	//app.loglevel(crow::LogLevel::Debug);	
 
-	CROW_ROUTE(app, "/")([]() {
-		//return "Hello world";
-		auto page = crow::mustache::load_text("helloworld.html");
-		return page;
-		});
+	// controllers/routes
+	HelloWorldController(app).AddRoutes();
 
-
+	// run the server
 	app.port(18080).multithreaded().run();
 
 	return 0;
+}
+
+void DefineRoutes(crow::SimpleApp& app)
+{
+	//app.loglevel(crow::LogLevel::Info);	
+
+	//CROW_ROUTE(app, "/helloworld")([]() {
+	//	//return "Hello world";
+	//	auto page = crow::mustache::load_text("helloworld.html");
+	//	return page;
+	//	});
+
+	//CROW_ROUTE(app, "/hellodesign/<string>")([](std::string designName) {
+	//	auto page = crow::mustache::load("helloworld.html");
+	//	crow::mustache::context ctx({ {"design", designName} });
+	//	return page.render(ctx);
+	//	});
+
+	CROW_ROUTE(app, "/steps/edadata/package_records")([]() {
+		
+		// /steps/edadata/package_records?design=sample_design
+		
+		auto page = crow::mustache::load_text("helloworld.html");
+		return page;
+		});
 }
