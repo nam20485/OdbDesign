b813a7b8a6f166d51d390c23ed209248a3a030e0
Nathan Miller <nmiller217@gmail.com>
2023-10-07 10:23:35 -0700

    refactor Build(...) methods to remove unncessary overload

--- OdbDesignLib/Design.h
+++ OdbDesignLib/Design.h
@@ -19,14 +19,17 @@ namespace Odb::Lib::ProductModel
 		Design();
 		~Design();
 
-		bool Build(std::string designDirectory);
-		bool Build(std::shared_ptr<FileModel::Design::FileArchive> pFileModel);		
+		bool Build(std::string path);
+		bool Build(std::shared_ptr<FileModel::Design::FileArchive> pFileModel);	
+
+		typedef std::vector<std::shared_ptr<Design>> Vector;
+		typedef std::map<std::string, std::shared_ptr<Design>> StringMap;
 
 	private:
 		std::string m_productModel;
 		std::string m_name;
 
-		std::string m_designDirectory;		
+		std::string m_path;		
 		std::shared_ptr<FileModel::Design::FileArchive> m_pFileModel;
 
 		Net::Vector m_nets;
@@ -39,8 +42,7 @@ namespace Odb::Lib::ProductModel
 		Component::StringMap m_componentsByName;
 		
 		Part::StringMap m_partsByName;
-
-		bool Build();
+		
 		bool BuildNets();
 		bool BuildPackages();
 		bool BuildParts();
