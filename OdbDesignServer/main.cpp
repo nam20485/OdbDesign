c984a6d820256155bb41a70086197c9ff401a78a
Nathan Miller <nmiller217@gmail.com>
2023-10-08 11:26:33 -0700

    rename file

---
+++ OdbDesignServer/main.cpp
@@ -0,0 +1,12 @@
+﻿// main.cpp : Source file for your target.
+//
+
+#include "OdbDesignServer.h"
+#include "OdbDesignServerApp.h"
+
+
+int main(int argc, char* argv[])
+{
+	Odb::App::Server::OdbDesignServerApp serverApp(argc, argv);
+	return (int) serverApp.Run();			
+}
