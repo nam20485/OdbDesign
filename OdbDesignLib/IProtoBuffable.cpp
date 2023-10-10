a4b33bf9ef23dd94b7fdf902c2b2f9b328f6373b
Nathan Miller <nmiller217@gmail.com>
2023-10-09 19:04:03 -0700

    remove unused classes and .cpp files

--- OdbDesignLib/IProtoBuffable.cpp
+++
@@ -1,1 +0,0 @@
-#include "IProtoBuffable.h"
--- OdbDesignLib/PbMessage.cpp
+++
@@ -1,8 +0,0 @@
-#include "PbMessage.h"
-#include <google/protobuf/util/json_util.h>
-#include <google/protobuf/stubs/stringpiece.h>
-
-
-namespace Odb::Lib
-{	
-}
