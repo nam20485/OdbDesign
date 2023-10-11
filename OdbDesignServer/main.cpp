68a47f4abfe79ed4e025e167107b322c920d9279
Nathan Miller <nmiller217@gmail.com>
2023-10-11 14:28:01 -0700

    ussing namespaces for brevity

--- OdbDesignServer/main.cpp
+++ OdbDesignServer/main.cpp
@@ -4,9 +4,10 @@
 #include "OdbDesignServer.h"
 #include "OdbDesignServerApp.h"
 
+using namespace Odb::App::Server;
 
 int main(int argc, char* argv[])
 {
-	Odb::App::Server::OdbDesignServerApp serverApp(argc, argv);
+	OdbDesignServerApp serverApp(argc, argv);
 	return (int) serverApp.Run();			
 }
