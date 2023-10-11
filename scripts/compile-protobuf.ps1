9fdf54544906c20ef1ec7e3e09aec082527873bc
Nathan Miller <nmiller217@gmail.com>
2023-10-11 09:17:18 -0700

    compile all *.proto files in the the protoc dir

--- scripts/compile-protobuf.ps1
+++ scripts/compile-protobuf.ps1
@@ -1,3 +1,3 @@
 $PROTOC = 'C:\Source\github\nam20485\OdbDesign\out\build\x64-debug\vcpkg_installed\x64-windows\tools\protobuf\protoc'
 
-. $PROTOC --cpp_out=../proto edadatafile.proto
+. $PROTOC --cpp_out=../proto *.proto
