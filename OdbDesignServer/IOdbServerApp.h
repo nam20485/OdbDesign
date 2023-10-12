db93c494be0d189a1c3fafd6832dee68f37cd22d
Nathan Miller <nmiller217@gmail.com>
2023-10-12 09:02:57 -0700

    create interface class for server apps

---
+++ OdbDesignServer/IOdbServerApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CommandLineArgs.h"
+#include "DesignCache.h"
+#include "ExitCode.h"
+#include "crow_win.h"
+
+namespace Odb::App::Server
+{
+	class IOdbServerApp
+	{
+	public:
+		virtual ~IOdbServerApp() {}
+
+		virtual const Utils::CommandLineArgs& arguments() const = 0;
+		virtual crow::SimpleApp& crow_app() = 0;
+		virtual Odb::Lib::DesignCache& design_cache() = 0;
+
+		virtual Utils::ExitCode Run() = 0;
+
+	protected:
+		// abstract class/interface
+		IOdbServerApp() = default;
+
+		virtual void register_routes() = 0;
+	};
+}
