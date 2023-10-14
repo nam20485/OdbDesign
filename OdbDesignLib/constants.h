56f33a8f1f40896818c71ad387667a504d29f06b
Nathan Miller <nmiller217@gmail.com>
2023-10-13 18:40:51 -0700

    add constants file

---
+++ OdbDesignLib/constants.h
@@ -0,0 +1,18 @@
+//
+// Created by nmill on 10/13/2023.
+//
+
+#ifndef ODBDESIGN_CONSTANTS_H
+#define ODBDESIGN_CONSTANTS_H
+
+namespace Odb::Lib::FileModel::Design
+{
+    class Constants
+    {
+    public:
+        inline static auto COMMENT_TOKEN = "#";
+
+    };
+}
+
+#endif //ODBDESIGN_CONSTANTS_H
