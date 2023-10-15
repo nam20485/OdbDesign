5a0c17433de5ab4581071e365837044f42673881
Nathan Miller <nmiller217@gmail.com>
2023-10-14 18:40:46 -0700

    implement parsing of Matrix file

--- OdbDesignLib/MatrixFile.cpp
+++ OdbDesignLib/MatrixFile.cpp
@@ -6,7 +6,7 @@
 #include <fstream>
 #include "str_trim.h"
 #include <string>
-#include "constants.h"
+#include "Constants.h"
 
 namespace Odb::Lib::FileModel::Design
 {
@@ -21,6 +21,10 @@ namespace Odb::Lib::FileModel::Design
         matrixFile.open(matrixFilePath, std::ios::in);
         if (!matrixFile.is_open()) return false;
 
+        std::shared_ptr<StepRecord> pCurrentStepRecord;
+        std::shared_ptr<LayerRecord> pCurrentLayerRecord;
+        bool openBraceFound = false;
+
         std::string line;
         while (std::getline(matrixFile, line))
         {
@@ -33,9 +37,303 @@ namespace Odb::Lib::FileModel::Design
                 {
                     // comment line
                 }
+                else if (line.find(StepRecord::RECORD_TOKEN))
+                {
+                    std::string token;
+                    if (!(lineStream >> token)) return false;
+                    if (token != StepRecord::RECORD_TOKEN) return false;
+
+                    if (lineStream >> token)
+                    {
+                        // open brace is at the end on the same line as the step array record open token
+                        if (token == Constants::ARRAY_RECORD_OPEN_TOKEN)
+                        {
+                            openBraceFound = true;
+
+                            // TODO: finish any existing previous step record
+                            // (same code as finding a close brace on an empty line)
+
+                            // open a new STEP array record
+                            pCurrentStepRecord = std::make_shared<StepRecord>();
+                        }
+                    }
+                }
+                else if (line.find(LayerRecord::RECORD_TOKEN))
+                {
+                    std::string token;
+                    if (!(lineStream >> token)) return false;
+                    if (token != LayerRecord::RECORD_TOKEN) return false;
+
+                    if (lineStream >> token)
+                    {
+                        // open brace is on same line as layer record open token
+                        if (token == Constants::ARRAY_RECORD_OPEN_TOKEN)
+                        {
+                            openBraceFound = true;
+
+                            // TODO: finish any existing previous layer or step record
+                            // (same code as finding a close brace on an empty line)
+
+                            // open a new LAYER array record
+                            pCurrentLayerRecord = std::make_shared<LayerRecord>();
+                        }
+                    }
+                }  
+                else if (line.find(Constants::ARRAY_RECORD_OPEN_TOKEN) == 0)
+                {
+                    // TODO: how to determine if you are opening a step or layer array record?
+                    // (maybe a boolean flag? stepArrayOpen = true/false)
+                    // no current opening of a layer or step array record found yet
+                    if (pCurrentStepRecord == nullptr && pCurrentLayerRecord == nullptr) return false;
+                    // found another open brace while still parsing after the previous record's open brace
+                    if (openBraceFound) return false;
+
+                    openBraceFound = true;
+
+                    // TODO: finish any existing previous layer or step record
+                    // (same code as finding a close brace on an empty line)
+
+                    // open a new LAYER array record
+                    //pCurrentLayerRecord = std::make_shared<LayerRecord>();
+                }
+                else if (line.find(Constants::ARRAY_RECORD_CLOSE_TOKEN) == 0)
+                {
+                    if (pCurrentStepRecord != nullptr && openBraceFound)
+                    {
+                        m_stepRecords.push_back(pCurrentStepRecord);
+                        pCurrentStepRecord.reset();
+                        openBraceFound = false;
+                    }
+                    else if (pCurrentLayerRecord != nullptr && openBraceFound)
+                    {
+                        m_layerRecords.push_back(pCurrentLayerRecord);
+                        pCurrentLayerRecord.reset();
+                        openBraceFound = false;
+                    }
+                    else
+                    {
+                        // found a close brace but aren't currently parsing a step or layer array record
+                        return false;
+                    }
+                }                
                 else
                 {
-                    return false;
+                    // it should be a name-value pair line (i.e. element of a step or layer array)
+                    std::string attribute;
+                    std::string value;
+
+                    if (!std::getline(lineStream, attribute, '=')) return false;
+                    if (!std::getline(lineStream, value)) return false;
+
+                    Utils::str_trim(attribute);
+                    Utils::str_trim(value);
+
+                    if (pCurrentStepRecord != nullptr && openBraceFound)
+                    {
+                        if (attribute == "COL" || attribute == "col")
+                        {
+                            pCurrentStepRecord->column = std::stoi(value);
+                        }
+                        else if (attribute == "NAME" || attribute == "name")
+                        {
+                            pCurrentStepRecord->name = value;
+                        }
+                        else if (attribute == "ID" || attribute == "id")
+                        {
+                            pCurrentStepRecord->id = (unsigned int) std::stoul(value);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+					else if (pCurrentLayerRecord != nullptr && openBraceFound)
+					{
+                        if (attribute == "ROW" || attribute == "row")
+                        {
+                            pCurrentLayerRecord->row = std::stoi(value);
+                        }
+                        else if (attribute == "NAME" || attribute == "name")
+                        {
+                            pCurrentLayerRecord->name = value;
+                        }
+                        else if (attribute == "ID" || attribute == "id")
+                        {
+                            pCurrentLayerRecord->id = (unsigned int)std::stoul(value);
+                        }
+                        else if (attribute == "TYPE" || attribute == "type")
+                        {
+                            if (value == "SIGNAL")
+                            {
+                                pCurrentLayerRecord->type = LayerRecord::Type::Signal;
+                            }
+                            else if (value == "POWER_GROUND")
+                            {
+                                pCurrentLayerRecord->type = LayerRecord::Type::PowerGround;
+							}
+							else if (value == "DIELECTRIC")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Dielectric;
+							}
+							else if (value == "MIXED")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Mixed;
+							}
+							else if (value == "SOLDE_RMASK")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::SolderMask;
+							}
+							else if (value == "SOLDER_PASTE")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::SolderPaste;
+							}
+							else if (value == "SILK_SCREEN")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::SilkScreen;
+							}
+							else if (value == "DRILL")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Drill;
+							}
+							else if (value == "ROUT")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Rout;
+							}
+							else if (value == "DOCUMENT")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Document;
+							}
+							else if (value == "COMPONENT")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Component;
+							}
+							else if (value == "MASK")
+							{
+								pCurrentLayerRecord->type = LayerRecord::Type::Mask;
+							}
+                            else if (value == "CONDUCTIVE_PASTE")
+                            {
+                                pCurrentLayerRecord->type = LayerRecord::Type::ConductivePaste;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else if (attribute == "CONTEXT" || attribute == "context")
+                        {
+                            if (value == "BOARD")
+                            {
+                                pCurrentLayerRecord->context = LayerRecord::Context::Board;
+                            }
+                            else if (value == "MISC")
+                            {
+                                pCurrentLayerRecord->context = LayerRecord::Context::Misc;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else if (attribute == "OLD_NAME" || attribute == "old_name")
+                        {
+                            pCurrentLayerRecord->oldName = value;
+                        }
+                        else if (attribute == "POLARITY" || attribute == "polarity")
+                        {
+                            if (value == "POSITIVE")
+                            {
+                                pCurrentLayerRecord->polarity = LayerRecord::Polarity::Positive;
+                            }
+                            else if (value == "NEGATIVE")
+                            {
+                                pCurrentLayerRecord->polarity = LayerRecord::Polarity::Negative;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else if (attribute == "DIELECTRIC_TYPE" || attribute == "dielectric_type")
+                        {
+                            if (value == "NONE")
+                            {
+								pCurrentLayerRecord->dielectricType = LayerRecord::DielectricType::None;
+							}
+                            else if (value == "PREPREG")
+                            {
+								pCurrentLayerRecord->dielectricType = LayerRecord::DielectricType::Prepreg;
+							}
+                            else if (value == "CORE")
+                            {
+								pCurrentLayerRecord->dielectricType = LayerRecord::DielectricType::Core;
+							}
+                            else
+                            {
+								return false;
+							}
+                        }
+                        else if (attribute == "DIELECTRIC_NAME" || attribute == "dielectric_name")
+                        {
+                            pCurrentLayerRecord->dielectricName = value;
+                        }
+                        else if (attribute == "FORM" || attribute == "form")
+                        {
+                            if (value == "RIGID")
+                            {
+                                pCurrentLayerRecord->form = LayerRecord::Form::Rigid;
+                            }
+                            else if (value == "FLEX")
+                            {
+								pCurrentLayerRecord->form = LayerRecord::Form::Flex;
+							}
+                            else
+                            {
+								return false;
+							}
+                        }
+                        else if (attribute == "CU_TOP" || attribute == "cu_top")
+                        {
+                            pCurrentLayerRecord->cuTop = std::stoul(value);
+                        }
+                        else if (attribute == "CU_TOP" || attribute == "cu_top")
+                        {
+                            pCurrentLayerRecord->cuTop = std::stoul(value);
+                        }
+                        else if (attribute == "CU_BOTTOM" || attribute == "cu_bottom")
+                        {
+							pCurrentLayerRecord->cuBottom = std::stoul(value);
+						}
+                        else if (attribute == "REF" || attribute == "ref")
+                        {
+                            pCurrentLayerRecord->ref = std::stoul(value);
+                        }
+                        else if (attribute == "START_NAME" || attribute == "start_name")
+                        {
+                            pCurrentLayerRecord->startName = value;
+                        }
+                        else if (attribute == "END_NAME" || attribute == "end_name")
+                        {
+                            pCurrentLayerRecord->endName = value;
+                        }
+                        else if (attribute == "ADD_TYPE" || attribute == "add_type")
+                        {
+                            pCurrentLayerRecord->addType = value;
+                        }
+                        else if (attribute == "COLOR" || attribute == "color")
+                        {
+                            if (!pCurrentLayerRecord->color.from_string(value)) return false;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+					}
+                    else
+                    {
+                        // found a name-value pair but aren't currently parsing a step or layer array record
+                        return false;
+                    }                    
                 }
             }
         }
--- OdbDesignLib/MatrixFile.h
+++ OdbDesignLib/MatrixFile.h
@@ -1,19 +1,18 @@
+#pragma once
+
 //
 // Created by nmill on 10/13/2023.
 //
-
-#ifndef ODBDESIGN_MATRIXFILE_H
-#define ODBDESIGN_MATRIXFILE_H
 
 #include "OdbFile.h"
 #include <filesystem>
 #include <vector>
 #include <memory>
+#include "RgbColor.h"
 
 
 namespace Odb::Lib::FileModel::Design
 {
-
     class MatrixFile : public OdbFile
     {
     public:
@@ -26,7 +25,9 @@ namespace Odb::Lib::FileModel::Design
             unsigned int id;
             std::string name;
 
-            typedef std::vector<std::shared_ptr<StepRecord>> Vector;		
+            typedef std::vector<std::shared_ptr<StepRecord>> Vector;	
+
+            inline static const char* RECORD_TOKEN = "STEP";
         };
 
         struct LayerRecord
@@ -73,26 +74,18 @@ namespace Odb::Lib::FileModel::Design
                 Flex
             };
                     
-            /// @class struct for representing preferred layer color
-            /// @brief each color value ranges from 0% - 100%            
-            /// noPreference is true if the Odb++ file does not specify a color
-            struct OdbRgbColor
-            {
-                bool noPreference;
-                uint8_t red;
-                uint8_t green;
-                uint8_t blue;
-            };
+            
 
             typedef std::vector<std::shared_ptr<LayerRecord>> Vector;
 
-            unsigned int row;
+            int row;
             Context context;
             Type type;
             std::string name;
             Polarity polarity;
             DielectricType dielectricType;
             std::string dielectricName;
+            Form form;
             unsigned int cuTop;
             unsigned int cuBottom;
             unsigned int ref;
@@ -100,8 +93,10 @@ namespace Odb::Lib::FileModel::Design
             std::string endName;
             std::string oldName;
             std::string addType;
-            OdbRgbColor color;
+            RgbColor color;
             unsigned int id;
+
+            inline static const char* RECORD_TOKEN = "LAYER";
 
         };
 
@@ -116,7 +111,4 @@ namespace Odb::Lib::FileModel::Design
         StepRecord::Vector m_stepRecords;
 
     };
-
 }
-
-#endif //ODBDESIGN_MATRIXFILE_H
