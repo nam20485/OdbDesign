611636d5b29a9dff3d59d4c02c1329b9e9f7e565
Nathan Miller <nmiller217@gmail.com>
2023-10-14 18:40:18 -0700

    why is constants.h lower case?

--- OdbDesignLib/constants.h
+++ OdbDesignLib/constants.h
@@ -1,16 +1,17 @@
+#pragma once
+
 //
 // Created by nmill on 10/13/2023.
 //
-
-#ifndef ODBDESIGN_CONSTANTS_H
-#define ODBDESIGN_CONSTANTS_H
 
 namespace Odb::Lib::FileModel::Design
 {
     class Constants
     {
     public:
-        inline static auto COMMENT_TOKEN = "#";
+        inline static const auto COMMENT_TOKEN = "#";
+        inline static const auto ARRAY_RECORD_OPEN_TOKEN = "{";
+        inline static const auto ARRAY_RECORD_CLOSE_TOKEN = "}";
 
     };
 }
