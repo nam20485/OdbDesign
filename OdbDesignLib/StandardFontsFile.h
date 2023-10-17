7455a0b79565b3404fdf0f07179c7eabca6c4a85
Nathan Miller <nmiller217@gmail.com>
2023-10-16 20:37:21 -0700

    implement parsing of fonts/standard file

--- OdbDesignLib/StandardFontsFile.h
+++ OdbDesignLib/StandardFontsFile.h
@@ -2,6 +2,7 @@
 
 #include "OdbFile.h"
 #include "odbdesign_export.h"
+#include "enums.h"
 
 namespace Odb::Lib::FileModel::Design
 {
@@ -13,8 +14,39 @@ namespace Odb::Lib::FileModel::Design
 	
 		bool Parse(std::filesystem::path path) override;
 
-	private:
+		struct CharacterBlock
+		{
+			struct LineRecord
+			{
+				float xStart;
+				float yStart;
+				float xEnd;
+				float yEnd;
+				Polarity polarity;
+				LineShape shape;
+				float width;
 
+				typedef std::vector<std::shared_ptr<LineRecord>> Vector;
+
+				inline static constexpr char* LINE_RECORD_TOKEN = "LINE";
+			};
+
+			inline static constexpr char* BEGIN_TOKEN = "CHAR";
+			inline static constexpr char* END_TOKEN = "ECHAR";
+
+			char character;
+			LineRecord::Vector m_lineRecords;
+
+			typedef std::vector<std::shared_ptr<CharacterBlock>> Vector;
+
+		};
+
+	private:
+		float m_xSize;
+		float m_ySize;
+		float m_offset;
+
+		CharacterBlock::Vector m_characterBlocks;
 
 	};
 }
