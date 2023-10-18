ecfa2431d530c85ec2be7c493e21303447fc950a
Nathan Miller <nmiller217@gmail.com>
2023-10-17 18:16:43 -0700

    fill in implementation of CommandLineArgs class

--- Utils/CommandLineArgs.h
+++ Utils/CommandLineArgs.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include "utils_export.h"
+#include <stdexcept>
+#include <map>
 
 
 namespace Utils
@@ -13,8 +15,18 @@ namespace Utils
 		CommandLineArgs(int argc, char* argv[]);
 		CommandLineArgs(const std::vector<std::string>& vecArgv);
 
+		bool boolArg(const std::string& name) const;
+		std::string stringArg(const std::string& name) const;
+		int intArg(const std::string& name) const;
+		double doubleArg(const std::string& name) const;
+
 	protected:
 		std::vector<std::string> m_vecArguments;
+		std::map<std::string, std::string> m_mapArguments;
 
-	};
+		std::string getArgValue(const std::string& name) const;
+
+		void parse();
+				
+	};	
 }
