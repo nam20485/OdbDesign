d04072055c74faa88209188bdfedb80c66731bd1
Nathan Miller <nmiller217@gmail.com>
2023-10-24 18:44:19 -0700

    fix windows header includes

--- OdbDesignLib/win.h
+++ OdbDesignLib/win.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
 
-#if defined (_MSC_VER)
-#	define _WIN32_WINNT 0x0601
+#include <WinSDKVer.h>
+
+#ifndef _WIN32_WINNT_WIN7
+#	define _WIN32_WINNT_WIN7	0x0601
 #endif
+
+#ifdef _MSC_VER
+#	define _WIN32_WINNT			_WIN32_WINNT_WIN7
+#	define WINVER				_WIN32_WINNT
+#endif
+
+#include <SDKDDKVer.h>
+//#include <windows.h>
