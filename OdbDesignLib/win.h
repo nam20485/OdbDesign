abd61612b4a699a7e8c54d29a354e38d7915da85
Nathan Miller <nmiller217@gmail.com>
2023-10-24 19:43:55 -0700

    guard against including windows header stuff if not using msc

--- OdbDesignLib/win.h
+++ OdbDesignLib/win.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-
-#include <WinSDKVer.h>
-
-#ifndef _WIN32_WINNT_WIN7
-#	define _WIN32_WINNT_WIN7	0x0601
-#endif
-
 #ifdef _MSC_VER
-#	define _WIN32_WINNT			_WIN32_WINNT_WIN7
-#	define WINVER				_WIN32_WINNT
-#endif
 
-#include <SDKDDKVer.h>
-//#include <windows.h>
+	#define WIN32_LEAN_AND_MEAN
+
+	#include <WinSDKVer.h>
+
+	#ifndef _WIN32_WINNT_WIN7
+	#	define _WIN32_WINNT_WIN7	0x0601
+	#endif
+
+	#define _WIN32_WINNT	_WIN32_WINNT_WIN7
+	#define WINVER			_WIN32_WINNT
+
+	#include <SDKDDKVer.h>
+	//#include <windows.h>
+
+#endif
