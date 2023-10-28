f2272f98bd59e56556e9d938321576fd3917cc6b
Nathan Miller <nmiller217@gmail.com>
2023-10-27 17:54:21 -0700

    create sha256sum's for binaries and upload them as release assets

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -1,35 +1,40 @@
 try {       
     const createResponse = await github.rest.repos.createRelease({
-    generate_release_notes: true,
-    name: process.env.RELEASE_NAME,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    tag_name: process.env.RELEASE_TAG,
-    body: require('fs').readFileSync('${{ github.workspace }}/release-body.md', 'utf8'),
-    target_commitish: '${{ github.ref_name }}'
-  });
+        generate_release_notes: true,
+        name: process.env.RELEASE_NAME,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        tag_name: process.env.RELEASE_TAG,
+        body: require('fs').readFileSync('${{ github.workspace }}/release/release-body.md', 'utf8'),
+        target_commitish: '${{ github.ref_name }}'
+    });
 
-  const filenames = [ 
-      'OdbDesign-Linux-x64.zip',
-      'OdbDesign-Windows-x64.zip',
-      'OdbDesign-MacOS-x64.zip'
-  ];
-  
-  for (const filename of filenames) {                  
-      const artifactsPath = '${{ github.workspace }}/artifacts';
-      const filePath = artifactsPath +'/' + filename;
-      const uploadResponse = await github.rest.repos.uploadReleaseAsset({
-          owner: context.repo.owner,
-          repo: context.repo.repo,
-          release_id: createResponse.data.id,        
-          name: filename,            
-          data: require('fs').readFileSync(filePath),
-          headers: {
-          'content-type': 'application/zip',
-          'content-length': require('fs').statSync(filePath).size
-          }    
-      });
-  }
+    const files =
+    [ 
+        { name: 'OdbDesign-Linux-x64.zip',  contentType: 'application/zip' },
+        { name: 'OdbDesign-Linux-x64.zip.sha256sum', contentType: 'text/plain' },
+        { name: 'OdbDesign-Windows-x64.zip', contentType: 'application/zip' },
+        { name: 'OdbDesign-Windows-x64.zip.sha256sum', contentType: 'text/plain' },
+        { name: 'OdbDesign-MacOS-x64.zip', contentType: 'application/zip' },
+        { name: 'OdbDesign-MacOS-x64.zip.sha256sum', contentType: 'text/plain' }      
+    ];
+
+    const artifactsPath = '${{ github.workspace }}/artifacts';
+    
+    for (const file of files) {                        
+        const filePath = artifactsPath +'/' + file.name;
+        const uploadResponse = await github.rest.repos.uploadReleaseAsset({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            release_id: createResponse.data.id,        
+            name: file.name,            
+            data: require('fs').readFileSync(filePath),
+            headers: {
+                'content-type': file.contentType,
+                'content-length': require('fs').statSync(filePath).size
+            }    
+        });
+    }
 } catch (error) {
     core.setFailed(error.message);
-}   
+}
