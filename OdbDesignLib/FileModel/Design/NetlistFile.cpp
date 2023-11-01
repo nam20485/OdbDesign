9a9487ceac1f0b7429f2a4055921a72821e610de
Nathan Miller <nmiller217@gmail.com>
2023-11-01 12:04:37 -0700

    parse net record structs out of netlist file records instead of just list of net names

--- OdbDesignLib/FileModel/Design/NetlistFile.cpp
+++ OdbDesignLib/FileModel/Design/NetlistFile.cpp
@@ -1,7 +1,11 @@
 #include "NetlistFile.h"
+#include "NetlistFile.h"
+#include "NetlistFile.h"
+#include "NetlistFile.h"
 #include <fstream>
 #include <sstream>
 #include <Logger.h>
+#include "../parse_error.h"
 
 namespace Odb::Lib::FileModel::Design
 {
@@ -12,6 +16,9 @@ namespace Odb::Lib::FileModel::Design
 
 	NetlistFile::~NetlistFile()
 	{
+		m_netRecords.clear();
+		m_netRecordsByName.clear();
+		m_netPointRecords.clear();
 	}
 
 	std::filesystem::path NetlistFile::GetPath() const
@@ -37,115 +44,236 @@ namespace Odb::Lib::FileModel::Design
 	NetlistFile::Staggered NetlistFile::GetStaggered() const
 	{
 		return m_staggered;
+	}	
+
+	const NetlistFile::NetRecord::Vector& NetlistFile::GetNetRecords() const
+	{
+		return m_netRecords;
 	}
 
-	const std::vector<std::string>& NetlistFile::GetNetNames() const
+	const NetlistFile::NetRecord::StringMap& NetlistFile::GetNetRecordsByName() const
 	{
-		return m_netNames;
+		return m_netRecordsByName;
+	}
+
+	const NetlistFile::NetPointRecord::Vector& NetlistFile::GetNetPointRecords() const
+	{
+		return m_netPointRecords;
 	}
 
 	bool NetlistFile::Parse()
 	{
-		m_name = std::filesystem::path(m_path).filename().string();
-
-		loginfo("Parsing netlist: " + m_name + "...");
-
-		auto netlistFilePath = m_path / "netlist";
-
-		if (!std::filesystem::exists(netlistFilePath)) return false;
-		else if (!std::filesystem::is_regular_file(netlistFilePath)) return false;
-
 		std::ifstream netlistFile;
-		netlistFile.open(netlistFilePath.string(), std::ios::in);
-		if (!netlistFile.is_open()) return false;
 
-		std::string line;
-		while (std::getline(netlistFile, line))
+		try
 		{
-			if (!line.empty())
+			m_name = std::filesystem::path(m_path).filename().string();
+
+			loginfo("Parsing netlist: " + m_name + "...");
+
+			auto netlistFilePath = m_path / "netlist";
+
+			if (!std::filesystem::exists(netlistFilePath))
 			{
-				std::stringstream lineStream(line);
+				auto message = "netlist file does not exist: [" + m_path.string() + "]";
+				throw std::exception(message.c_str());
+			}
+			else if (!std::filesystem::is_regular_file(netlistFilePath))
+			{
+				auto message = "netlist file is not a file: [" + m_path.string() + "]";
+				throw std::exception(message.c_str());
+			}
+			
+			netlistFile.open(netlistFilePath.string(), std::ios::in);
+			if (!netlistFile.is_open())
+			{
+				auto message = "unable to open netlist file: [" + m_path.string() + "]";
+				throw std::exception(message.c_str());
+			}
 
-				if (line.find(COMMENT_TOKEN) == 0)
+			int lineNumber = 0;
+			std::string line;
+			while (std::getline(netlistFile, line))
+			{
+				lineNumber++;
+				if (!line.empty())
 				{
-					// comment line
-				}
-				else if (line.find(UNITS_TOKEN) == 0)
-				{
-					// units line
-					std::string token;
-					if (!std::getline(lineStream, token, '=')) return false;
-					else if (!std::getline(lineStream, token, '=')) return false;
-					m_units = token;
-				}
-				else if (line.find(HEADER_TOKEN) == 0)
-				{
-					// header line
-					std::string token;
+					std::stringstream lineStream(line);
 
-					// H (throw away
-					lineStream >> token;
-
-					// optimize(d)
-					lineStream >> token;
-					if (token != "optimize") return false;
-					char optimized;
-					lineStream >> optimized;
-					m_optimized = (std::tolower(optimized) == 'y');
-
-					// staggered (optional)
-					if (lineStream >> token &&
-						token == "staggered")
+					if (line.find(COMMENT_TOKEN) == 0)
 					{
-						char staggered;
-						lineStream >> staggered;
-						if (std::tolower(staggered) == 'y')
+						// comment line
+					}
+					else if (line.find(UNITS_TOKEN) == 0)
+					{
+						// units line
+						std::string token;
+						if (!std::getline(lineStream, token, '='))
 						{
-							m_staggered = Staggered::Yes;
+							throw_parse_error(m_path, line, token, lineNumber);
 						}
-						else if (std::tolower(staggered) == 'n')
+						else if (!std::getline(lineStream, token, '='))
 						{
-							m_staggered = Staggered::No;
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+						m_units = token;
+					}
+					else if (line.find(HEADER_TOKEN) == 0)
+					{
+						// header line
+						std::string token;
+
+						// H (throw away
+						lineStream >> token;
+
+						// optimize(d)
+						lineStream >> token;
+						if (token != "optimize")
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						char optimized;						
+						if (!(lineStream >> optimized))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						if (optimized == 'Y' || std::tolower(optimized) == 'y')
+						{
+							m_optimized = true;
+						}
+						else if (optimized == 'N' || std::tolower(optimized) == 'n')
+						{
+							m_optimized = false;
+						}
+						else
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						// staggered (optional)
+						if (lineStream >> token && token == "staggered")
+						{
+							char staggered;
+							if (!(lineStream >> staggered))
+							{
+								throw_parse_error(m_path, line, token, lineNumber);
+							}
+							
+							if (staggered == 'Y' || std::tolower(staggered) == 'y')
+							{
+								m_staggered = Staggered::Yes;
+							}
+							else if (staggered == 'N' || std::tolower(staggered) == 'n')
+							{
+								m_staggered = Staggered::No;
+							}
+							else
+							{
+								throw_parse_error(m_path, line, token, lineNumber);
+							}
 						}
 						else
 						{
 							m_staggered = Staggered::Unknown;
 						}
 					}
-					else
+					else if (line.find(NetRecord::FIELD_TOKEN) == 0)
 					{
-						m_staggered = Staggered::Unknown;
+						// net record line						
+
+						// net serial number
+						std::string token;
+						if (!(lineStream >> token))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						auto strSerialNumber = token;
+						strSerialNumber.erase(0, 1); // remove leading '$'
+						auto ulSerialNumber = std::stoul(strSerialNumber);
+						if (ulSerialNumber > UINT_MAX)
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}			
+
+						auto unSerialNumber = static_cast<unsigned int>(ulSerialNumber);
+
+						// net name
+						if (!(lineStream >> token))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						auto netName = token;
+
+						auto pNetRecord = std::make_shared<NetRecord>();
+						pNetRecord->serialNumber = unSerialNumber;
+						pNetRecord->netName = netName;
+						m_netRecords.push_back(pNetRecord);
+						m_netRecordsByName[pNetRecord->netName] = pNetRecord;						
+					}
+					else // NetPointRecord (starts with an unsigned int, netNumber)
+					{
+						std::string token;
+						if (!(lineStream >> token))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						auto ulNetNumber = std::stoul(token);
+						if (ulNetNumber >= UINT_MAX)
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						auto unNetNumber = static_cast<unsigned int>(ulNetNumber);
+
+						auto pNetPointRecord = std::make_shared<NetPointRecord>();
+						pNetPointRecord->netNumber = unNetNumber;
+
+						if (!(lineStream >> pNetPointRecord->radius))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						if (!(lineStream >> pNetPointRecord->x))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						if (!(lineStream >> pNetPointRecord->y))
+						{
+							throw_parse_error(m_path, line, token, lineNumber);
+						}
+
+						// TODO: parse the rest of the NetPointRecord fields
+
+						m_netPointRecords.push_back(pNetPointRecord);																											
 					}
 				}
-				else if (line.find(NET_TOKEN) == 0)
-				{
-					// net record line
-
-					// net number
-					std::string strNetNumber;
-					if (!(lineStream >> strNetNumber)) return false;
-					//strNetNumber.erase(0, 1); // remove leading '$'
-					//unsigned int netNumber = std::stoul(strNetNumber);
-					// don't use net number to specify index of net in vector,
-					// order of parsing/occurence in file dictates net number
-					// since we use vecotr.push_back() to add net names to vector
-
-					// net name
-					std::string netName;
-					if (!(lineStream >> netName)) return false;
-					m_netNames.push_back(netName);
-				}
-				else // if (starts with a (signed) int, including -1)
-				{
-					// netlist points record
-					// TODO: parse netlist points
-				}
 			}
+
+			loginfo("Parsing netlist: " + m_name + " complete");
+
+			netlistFile.close();
 		}
+		catch (parse_error& pe)
+		{
+			auto m = pe.getParseInfo().toString("Parse Error:");
+			logerror(m);
+			// cleanup file
+			netlistFile.close();
+			throw pe;
 
-		loginfo("Parsing netlist: " + m_name + " complete");
-
-		netlistFile.close();
+		}
+		catch (std::exception& e)
+		{
+			logexception(e);
+			netlistFile.close();
+			throw e;
+		}
 
 		return true;
 	}
