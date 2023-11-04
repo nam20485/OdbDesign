d819adaa7276ca6b3e86e793820c544690aaf963
Nathan Miller <nmiller217@gmail.com>
2023-11-03 20:00:25 -0700

    revert previous change and then cast argument to std::isspace(char) to unsigned char since it can only handle values c such that c = [0,255] or it will throw assertion

--- Utils/str_trim.cpp
+++ Utils/str_trim.cpp
@@ -6,38 +6,41 @@
 
 namespace Utils
 {
-    //// trim from start (in place)
-    //std::string& str_ltrim(std::string& s)
-    //{
-    //    auto it = std::find_if(s.begin(), s.end(),
-    //        [](char c) {
-    //            return !std::isspace(c);
-    //        });
-    //    s.erase(s.begin(), it);
-    //    return s;
-    //}
+    //! std::isspace(char) can only handle chars in the range [0,255] 
+    //! so we need to cast the it's argument to unsigned char
 
-    //// trim from end (in place)
-    //std::string& str_rtrim(std::string& s)
-    //{
-    //    auto it = std::find_if(s.rbegin(), s.rend(),
-    //        [](char c) {
-    //            return !std::isspace(c);
-    //        });
-    //    s.erase(it.base(), s.end());
-    //    return s;
-    //}
-
-    //// trim from both ends (in place)
-    //std::string& str_trim(std::string& s)
-    //{
-    //    return str_ltrim(str_rtrim(s));
-    //}
-
-    std::string& str_ltrim(std::string& s, char toRemove)
+    // trim from start (in place)
+    std::string& str_ltrim(std::string& s)
     {
         auto it = std::find_if(s.begin(), s.end(),
-            [toRemove](char c) {
+            [](unsigned char c) {
+                return !std::isspace(c);
+            });
+        s.erase(s.begin(), it);
+        return s;
+    }
+
+    // trim from end (in place)
+    std::string& str_rtrim(std::string& s)
+    {
+        auto it = std::find_if(s.rbegin(), s.rend(),
+            [](unsigned char c) {
+                return !std::isspace(c);
+            });
+        s.erase(it.base(), s.end());
+        return s;
+    }
+
+    // trim from both ends (in place)
+    std::string& str_trim(std::string& s)
+    {
+        return str_ltrim(str_rtrim(s));
+    }
+
+    std::string& str_ltrim(std::string& s, /*unsigned*/ char toRemove)
+    {
+        auto it = std::find_if(s.begin(), s.end(),
+            [toRemove](/*unsigned*/ char c) {
                 return c != toRemove;
             });
         s.erase(s.begin(), it);
@@ -45,10 +48,10 @@ namespace Utils
     }
 
     // trim from end (in place)
-    std::string& str_rtrim(std::string& s, char toRemove)
+    std::string& str_rtrim(std::string& s, /*unsigned*/ char toRemove)
     {
         auto it = std::find_if(s.rbegin(), s.rend(),
-            [toRemove](char c) {
+            [toRemove](/*unsigned*/ char c) {
                 return c != toRemove;
             });
         s.erase(it.base(), s.end());
@@ -56,47 +59,47 @@ namespace Utils
     }
 
     // trim from both ends (in place)
-    std::string& str_trim(std::string& s, char toRemove)
+    std::string& str_trim(std::string& s, /*unsigned*/ char toRemove)
     {
         return str_ltrim(str_rtrim(s, toRemove), toRemove);
     }
 
-    //// trim from start (copying)
-    //std::string str_ltrim_copy(std::string s)
-    //{
-    //    auto copy(s);
-    //    return str_ltrim(copy);
-    //}
+    // trim from start (copying)
+    std::string str_ltrim_copy(std::string s)
+    {
+        auto copy(s);
+        return str_ltrim(copy);
+    }
 
-    //// trim from end (copying)
-    //std::string str_rtrim_copy(std::string s)
-    //{
-    //    auto copy(s);
-    //    return str_rtrim(copy);
-    //}
+    // trim from end (copying)
+    std::string str_rtrim_copy(std::string s)
+    {
+        auto copy(s);
+        return str_rtrim(copy);
+    }
 
-    //// trim from both ends (copying)
-    //std::string str_trim_copy(std::string s)
-    //{
-    //    auto copy(s);
-    //    return str_trim(copy);
-    //}
+    // trim from both ends (copying)
+    std::string str_trim_copy(std::string s)
+    {
+        auto copy(s);
+        return str_trim(copy);
+    }
 
-    std::string str_ltrim_copy(std::string s, char toRemove)
+    std::string str_ltrim_copy(std::string s, /*unsigned*/ char toRemove)
     {
         auto copy(s);
         return str_ltrim(copy, toRemove);
     }
 
     // trim from end (copying)
-    std::string str_rtrim_copy(std::string s, char toRemove)
+    std::string str_rtrim_copy(std::string s, /*unsigned*/ char toRemove)
     {
         auto copy(s);
         return str_rtrim(copy, toRemove);
     }
 
     // trim from both ends (copying)
-    std::string str_trim_copy(std::string s, char toRemove)
+    std::string str_trim_copy(std::string s, /*unsigned*/ char toRemove)
     {
         auto copy(s);
         return str_trim(copy, toRemove);
