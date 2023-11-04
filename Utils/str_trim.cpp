c67733cfaff3f5246726ca55b0ff2044736b17c6
Nathan Miller <nmiller217@gmail.com>
2023-11-03 19:42:08 -0700

    use comparison to a space char for trimming instead of std::_isspace(char) and make it the default when another char to trim is not passed in (fixes assertion that unicode characters are not chars)

--- Utils/str_trim.cpp
+++ Utils/str_trim.cpp
@@ -6,33 +6,33 @@
 
 namespace Utils
 {
-    // trim from start (in place)
-    std::string& str_ltrim(std::string& s)
-    {
-        auto it = std::find_if(s.begin(), s.end(),
-            [](char c) {
-                return !std::isspace(c);
-            });
-        s.erase(s.begin(), it);
-        return s;
-    }
+    //// trim from start (in place)
+    //std::string& str_ltrim(std::string& s)
+    //{
+    //    auto it = std::find_if(s.begin(), s.end(),
+    //        [](char c) {
+    //            return !std::isspace(c);
+    //        });
+    //    s.erase(s.begin(), it);
+    //    return s;
+    //}
 
-    // trim from end (in place)
-    std::string& str_rtrim(std::string& s)
-    {
-        auto it = std::find_if(s.rbegin(), s.rend(),
-            [](char c) {
-                return !std::isspace(c);
-            });
-        s.erase(it.base(), s.end());
-        return s;
-    }
+    //// trim from end (in place)
+    //std::string& str_rtrim(std::string& s)
+    //{
+    //    auto it = std::find_if(s.rbegin(), s.rend(),
+    //        [](char c) {
+    //            return !std::isspace(c);
+    //        });
+    //    s.erase(it.base(), s.end());
+    //    return s;
+    //}
 
-    // trim from both ends (in place)
-    std::string& str_trim(std::string& s)
-    {
-        return str_ltrim(str_rtrim(s));
-    }
+    //// trim from both ends (in place)
+    //std::string& str_trim(std::string& s)
+    //{
+    //    return str_ltrim(str_rtrim(s));
+    //}
 
     std::string& str_ltrim(std::string& s, char toRemove)
     {
@@ -61,26 +61,26 @@ namespace Utils
         return str_ltrim(str_rtrim(s, toRemove), toRemove);
     }
 
-    // trim from start (copying)
-    std::string str_ltrim_copy(std::string s)
-    {
-        auto copy(s);
-        return str_ltrim(copy);
-    }
+    //// trim from start (copying)
+    //std::string str_ltrim_copy(std::string s)
+    //{
+    //    auto copy(s);
+    //    return str_ltrim(copy);
+    //}
 
-    // trim from end (copying)
-    std::string str_rtrim_copy(std::string s)
-    {
-        auto copy(s);
-        return str_rtrim(copy);
-    }
+    //// trim from end (copying)
+    //std::string str_rtrim_copy(std::string s)
+    //{
+    //    auto copy(s);
+    //    return str_rtrim(copy);
+    //}
 
-    // trim from both ends (copying)
-    std::string str_trim_copy(std::string s)
-    {
-        auto copy(s);
-        return str_trim(copy);
-    }
+    //// trim from both ends (copying)
+    //std::string str_trim_copy(std::string s)
+    //{
+    //    auto copy(s);
+    //    return str_trim(copy);
+    //}
 
     std::string str_ltrim_copy(std::string s, char toRemove)
     {
