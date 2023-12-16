075678053761465d2d7feb420fd9751d9887ff63
Nathan Miller <nmiller217@gmail.com>
2023-12-16 08:12:51 -0800

    add more to SymbolsDirectory

--- OdbDesignLib/FileModel/Design/SymbolsDirectory.h
+++ OdbDesignLib/FileModel/Design/SymbolsDirectory.h
@@ -3,10 +3,20 @@
 #include "../../odbdesign_export.h"
 #include "FeaturesFile.h"
 #include "AttrListFile.h"
+#include <vector>
+#include <map>
+#include <string>
+#include <memory>
 
 namespace Odb::Lib::FileModel::Design
 {
 	class ODBDESIGN_EXPORT SymbolsDirectory
 	{
+	public:
+		typedef std::vector<std::shared_ptr<SymbolsDirectory>> Vector;
+		typedef std::map<std::string, std::shared_ptr<SymbolsDirectory>> StringMap;
+
+	private:
+
 	};
 }
