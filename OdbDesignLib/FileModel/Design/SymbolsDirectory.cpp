9107bcb68afaa9dd8b932c965289a902d3f18832
Nathan Miller <nmiller217@gmail.com>
2023-12-16 09:17:29 -0800

    complete implementation of SymbolsDirectory class

--- OdbDesignLib/FileModel/Design/SymbolsDirectory.cpp
+++ OdbDesignLib/FileModel/Design/SymbolsDirectory.cpp
@@ -1,6 +1,46 @@
 #include "SymbolsDirectory.h"
+#include "Logger.h"
+#include <filesystem>
 
 namespace Odb::Lib::FileModel::Design
 {
+	SymbolsDirectory::SymbolsDirectory(const std::filesystem::path& path)
+		: m_name("")
+		, m_path(path)
+	{
+	}
 
+	bool SymbolsDirectory::Parse()
+	{
+
+		if (!std::filesystem::exists(m_path)) return false;
+		else if (!std::filesystem::is_directory(m_path)) return false;
+
+		m_name = std::filesystem::path(m_path).filename().string();
+
+		loginfo("Parsing symbols directory: " + m_name + "...");
+
+		if (!ParseFeaturesFile(m_path)) return false;
+		if (!ParseAttrListFile(m_path)) return false;
+
+		loginfo("Parsing symbols directory: " + m_name + " complete");
+
+		return true;
+	}
+
+	std::string SymbolsDirectory::GetName() const { return m_name; }
+	std::filesystem::path SymbolsDirectory::GetPath() const { return m_path; }
+
+	const FeaturesFile& SymbolsDirectory::GetFeaturesFile() const { return m_featuresFile; }
+	const AttrListFile& SymbolsDirectory::GetAttrListFile() const { return m_attrListFile; }
+	
+	bool SymbolsDirectory::ParseFeaturesFile(const std::filesystem::path& directory)
+	{
+		return m_featuresFile.Parse(directory);
+	}
+
+	bool SymbolsDirectory::ParseAttrListFile(const std::filesystem::path& directory)
+	{
+		return m_attrListFile.Parse(directory);
+	}
 }
--- OdbDesignLib/FileModel/Design/SymbolsDirectory.h
+++ OdbDesignLib/FileModel/Design/SymbolsDirectory.h
@@ -7,16 +7,35 @@
 #include <map>
 #include <string>
 #include <memory>
+#include <filesystem>
 
 namespace Odb::Lib::FileModel::Design
 {
 	class ODBDESIGN_EXPORT SymbolsDirectory
 	{
 	public:
+		SymbolsDirectory(const std::filesystem::path& path);
+
 		typedef std::vector<std::shared_ptr<SymbolsDirectory>> Vector;
 		typedef std::map<std::string, std::shared_ptr<SymbolsDirectory>> StringMap;
 
+		virtual bool Parse();
+
+		std::string GetName() const;
+		std::filesystem::path GetPath() const;
+
+		const FeaturesFile& GetFeaturesFile() const;
+		const AttrListFile& GetAttrListFile() const;
+
 	private:
+		std::string m_name;		
+		std::filesystem::path m_path;
+
+		FeaturesFile m_featuresFile;
+		AttrListFile m_attrListFile;
+
+		bool ParseFeaturesFile(const std::filesystem::path& directory);
+		bool ParseAttrListFile(const std::filesystem::path& directory);
 
 	};
 }
