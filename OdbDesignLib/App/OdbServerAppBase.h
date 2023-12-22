ee58737b9a865b305ba3383ef681756f88b45ac7
Nathan Miller <nmiller217@gmail.com>
2023-12-22 10:17:10 -0800

    add http server interface classes to CMake

--- OdbDesignLib/App/OdbServerAppBase.h
+++ OdbDesignLib/App/OdbServerAppBase.h
@@ -4,6 +4,7 @@
 #include "OdbAppBase.h"
 #include "RouteController.h"
 #include "../odbdesign_export.h"
+#include "IHttpServer.h"
 
 namespace Odb::Lib::App
 {
@@ -14,17 +15,20 @@ namespace Odb::Lib::App
 		virtual ~OdbServerAppBase();
 
 		crow::SimpleApp& crow_app() override;
+		IHttpServer& http_server() override;
 
 		Utils::ExitCode Run() override;		
 
 	protected:
-		crow::SimpleApp m_crowApp;
 		RouteController::Vector m_vecControllers;
 
 		// implement in subclasses to add route controllers
 		virtual void add_controllers() = 0;
 
 	private:
+		crow::SimpleApp m_crowApp;
+		IHttpServer m_httpServer;
+
 		void register_routes();
 
 		static constexpr const char* SSL_CERT_FILE = "ssl.crt";
