6416d845217e8c14becfe0bb305c7f8fa1760687
Nathan Miller <nmiller217@gmail.com>
2023-12-23 15:51:43 -0800

    make m_crowApp private

--- OdbDesignLib/App/OdbServerAppBase.h
+++ OdbDesignLib/App/OdbServerAppBase.h
@@ -17,14 +17,15 @@ namespace Odb::Lib::App
 
 		Utils::ExitCode Run() override;		
 
-	protected:
-		crow::SimpleApp m_crowApp;
+	protected:		
 		RouteController::Vector m_vecControllers;
 
 		// implement in subclasses to add route controllers
 		virtual void add_controllers() = 0;
 
 	private:
+		crow::SimpleApp m_crowApp;
+
 		void register_routes();
 
 		static constexpr const char* SSL_CERT_FILE = "ssl.crt";
