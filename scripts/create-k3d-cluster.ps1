aecec7f62ea252e873c16e8db880956be7393dd2
Nathan Miller <nmiller217@gmail.com>
2023-12-28 11:27:26 -0800

    refactor create k3d cluster script

--- scripts/create-k3d-cluster.ps1
+++ scripts/create-k3d-cluster.ps1
@@ -1,28 +1,44 @@
 # When set to true, the cluster will be deleted first
-# [Parameter()]
-# [Boolean]$DeleteClusterFirst = $false
-
-param(
-    [switch]$DeleteClusterFirst = $false
+param(    
+    [string]$ClusterName = "k3dcluster",    
+    [int]$NumAgents = 3,
+    [int]$IngressHostPort = 8081,
+    [switch]$DeleteClusterFirst = $false,
+    [switch]$ForceDelete = $false
 )
 
-$clusterName="k3dcluster"
-$hostIp="10.0.0.185"
-$numAgents=3
-$ingressHostPort=8081
+# $clusterName="k3dcluster"
+$hostIp = "10.0.0.185"
+# $numAgents=3
+# $ingressHostPort=8081
 
 if ($DeleteClusterFirst) {
 
-    $delete = Read-Host "Type cluster name and hit ENTER to delete the cluster first ($clusterName)..."
-    if ($delete -ne $clusterName) {
-        Write-Host "Cluster name did not match. Exiting..."
-        Exit
+    if (! $ForceDelete) {`
+        $clusterNameInput = Read-Host "Type cluster name ($ClusterName) and hit ENTER to delete the cluster first..."
     }
-    else
-    {
-        k3d cluster delete $clusterName
+    else {
+        Write-Host "Force delete specified..."
+        $clusterNameInput = $ClusterName
+    }
+    
+    if ($clusterNameInput -ne $ClusterName) {
+        Write-Host "Cluster name did not match. Exiting..."
+        Exit 1
+    }
+    else {
+        Write-Host "Deleting cluster $ClusterName..."
+        k3d cluster clusterNameInput$clusterNameInput $ClusterName
+        Write-Host "Cluster $ClusterName deleted."
     }
 }
 
-k3d cluster create $clusterName --agents $numAgents --k3s-arg="--tls-san=$hostIp@server:0" -p "${ingressHostPort}:80@loadbalancer"
+Write-Host "Creating cluster $ClusterName..."
 
+k3d cluster create $ClusterName `
+    --agents $NumAgents `
+    --k3s-arg="--tls-san=$hostIp@server:0" `
+    --k3s-arg="--tls-san=precision5820@server:0" `
+    -p "${IngressHostPort}:80@loadbalancer"
+
+Write-Host "Cluster $ClusterName created."
