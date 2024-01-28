364a7bcd7547db2e781bb003a4d545d137229bf2
Nathan Miller <nmiller217@gmail.com>
2024-01-28 09:09:40 -0800

    implement /designs endpoints with empty route handlers

--- OdbDesignServer/Controllers/DesignsController.cpp
+++ OdbDesignServer/Controllers/DesignsController.cpp
@@ -1,13 +1,244 @@
 #include "DesignsController.h"
+#include <JsonCrowReturnable.h>
+#include "UrlEncoding.h"
+
+using namespace Odb::Lib::App;
+using namespace Utils;
 
 namespace Odb::App::Server
 {
-	DesignsController::DesignsController(Odb::Lib::App::IOdbServerApp& serverApp)
+	DesignsController::DesignsController(IOdbServerApp& serverApp)
 		: RouteController(serverApp)
 	{
 	}
 
 	void DesignsController::register_routes()
 	{
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs")
+			([&](const crow::request& req)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_list_route_handler(req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>")
+			([&](const crow::request& req, std::string designName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->design_route_handler(designName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/components")
+			([&](const crow::request& req, std::string designName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_components_route_handler(designName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/components/<string>")
+			([&](const crow::request& req, std::string designName, std::string refDes)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_component_route_handler(designName, refDes, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/nets")
+			([&](const crow::request& req, std::string designName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_nets_route_handler(designName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/nets/<string>")
+			([&](const crow::request& req, std::string designName, std::string netName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_net_route_handler(designName, netName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/packages")
+			([&](const crow::request& req, std::string designName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_packages_route_handler(designName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/packages/<string>")
+			([&](const crow::request& req, std::string designName, std::string packageName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_package_route_handler(designName, packageName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/parts")
+			([&](const crow::request& req, std::string designName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_parts_route_handler(designName, req);
+				});
+
+		CROW_ROUTE(m_serverApp.crow_app(), "/designs/<string>/parts/<string>")
+			([&](const crow::request& req, std::string designName, std::string partName)
+				{
+					// authenticate request before sending to handler
+					auto authResp = m_serverApp.request_auth().AuthenticateRequest(req);
+					if (authResp.code != crow::status::OK)
+					{
+						return authResp;
+					}
+
+					return this->designs_part_route_handler(designName, partName, req);
+				});		
+	}
+
+	crow::response DesignsController::designs_list_route_handler(const crow::request& req)
+	{
+		const auto& fileArchives = m_serverApp.designs().getUnloadedDesignNames();
+
+		crow::json::wvalue::list designNames;
+		for (const auto& designName : fileArchives)
+		{
+			designNames.push_back(designName);
+		}
+
+		crow::json::wvalue jsonResponse;
+		jsonResponse["designs"] = std::move(designNames);
+		return crow::response(jsonResponse);
+	}
+
+	crow::response DesignsController::design_route_handler(std::string designName, const crow::request& req)
+	{
+		auto designNameDecoded = UrlEncoding::decode(designName);
+		if (designNameDecoded.empty())
+		{
+			return crow::response(crow::status::BAD_REQUEST, "design name not specified");
+		}
+
+		auto pDesign = m_serverApp.designs().GetDesign(designNameDecoded);
+		if (pDesign == nullptr)
+		{
+			std::stringstream ss;
+			ss << "design: \"" << designNameDecoded << "\" not found";
+			return crow::response(crow::status::NOT_FOUND, ss.str());
+		}
+
+		return crow::response(JsonCrowReturnable(*pDesign));
+	}
+	crow::response DesignsController::designs_components_route_handler(std::string designName, const crow::request& req)
+	{
+		auto designNameDecoded = UrlEncoding::decode(designName);
+		if (designNameDecoded.empty())
+		{
+			return crow::response(crow::status::BAD_REQUEST, "design name not specified");
+		}
+
+		auto pDesign = m_serverApp.designs().GetDesign(designNameDecoded);
+		if (pDesign == nullptr)
+		{
+			std::stringstream ss;
+			ss << "design: \"" << designNameDecoded << "\" not found";
+			return crow::response(crow::status::NOT_FOUND, ss.str());
+		}
+
+		std::vector<crow::json::rvalue> rvComponents;
+		
+		const auto& components = pDesign->GetComponents();
+		for (const auto& component : components)
+		{
+			rvComponents.push_back(crow::json::load(component->to_json()));
+		}
+
+		crow::json::wvalue wv;
+		wv["components"] = std::move(rvComponents);
+		return crow::response(wv);
+	}
+
+	crow::response DesignsController::designs_component_route_handler(std::string designName, std::string refDes, const crow::request& req)
+	{
+		return crow::response();
+	}
+
+	crow::response DesignsController::designs_nets_route_handler(std::string designName, const crow::request& req)
+	{
+		return crow::response();
+	}
+
+	crow::response DesignsController::designs_net_route_handler(std::string designName, std::string netName, const crow::request& req)
+	{
+		return crow::response();
+	}
+
+	crow::response DesignsController::designs_parts_route_handler(std::string designName, const crow::request& req)
+	{
+		return crow::response();
+	}
+
+	crow::response DesignsController::designs_part_route_handler(std::string designName, std::string partName, const crow::request& req)
+	{
+		return crow::response();
+	}
+
+	crow::response DesignsController::designs_packages_route_handler(std::string designName, const crow::request& req)
+	{
+		return crow::response();
+	}
+
+	crow::response DesignsController::designs_package_route_handler(std::string designName, std::string packageName, const crow::request& req)
+	{
+		return crow::response();
 	}
 }
