049ee5a9e2d14edf85e9b3f5d3d72afc355ddd2b
Nathan Miller <nmiller217@gmail.com>
2024-01-29 07:23:37 -0800

    fix warning

--- OdbDesignTests/Fixtures/FileArchiveLoadFixture.cpp
+++ OdbDesignTests/Fixtures/FileArchiveLoadFixture.cpp
@@ -32,7 +32,7 @@ namespace Odb::Test::Fixtures
 	std::string FileArchiveLoadFixture::getTestDataDir()
 	{
 		auto szTestDataDir = std::getenv(ODB_TEST_DATA_DIR_ENV_NAME);
-		if (szTestDataDir == nullptr) szTestDataDir = "";
+		if (szTestDataDir == nullptr) return "";
 		//if (szTestDataDir == nullptr) throw std::runtime_error("ODB_TEST_DATA_DIR environment variable is not set");				
 		//if (!exists(szTestDataDir)) throw std::runtime_error("ODB_TEST_DATA_DIR environment variable is set to a non-existent directory");
 		return szTestDataDir;
