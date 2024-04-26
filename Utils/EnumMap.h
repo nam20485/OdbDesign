7ba31b9dca639ba9e037a814a2fa0a296421f6b8
Nathan Miller <nmiller217@gmail.com>
2024-04-26 09:52:34 -0700

    add contains() methods to EnumMap

--- Utils/EnumMap.h
+++ Utils/EnumMap.h
@@ -3,6 +3,18 @@
 #include <vector>
 #include <string>
 #include <exception>
+#include <cctype>
+
+//bool case_insensitive_compare(const std::string& s1, const std::string& s2)
+//{
+//	return std::equal(
+//		s1.begin(), s1.end(),
+//		s2.begin(), s2.end(),
+//		[](char c1, char c2)
+//		{
+//			return std::tolower(c1) == std::tolower(c2);
+//		});
+//}
 
 template<typename E>
 class EnumMap
@@ -24,12 +36,25 @@ public:
 
 	E getValue(const std::string& name) const
 	{
-		auto find = std::find(m_names.begin(), m_names.end(), name);
-		if (find == m_names.end())
+		//auto find = std::find_if(m_names.begin(), m_names.end(), name, case_insensitive_compare);
+		auto findIt = std::find(m_names.begin(), m_names.end(), name);
+		if (findIt == m_names.end())
 		{
 			throw Exception("no value found for name");
 		}
-		return static_cast<E>(std::distance(m_names.begin(), find));
+		return static_cast<E>(std::distance(m_names.begin(), findIt));
+	}
+
+	bool contains(const std::string& name) const
+	{
+		//return std::find_if(m_names.begin(), m_names.end(), name, case_insensitive_compare) != m_names.end();
+		return std::find(m_names.begin(), m_names.end(), name) != m_names.end();
+	}
+
+	bool contains(const E e) const
+	{
+		auto index = static_cast<size_t>(e);
+		return index >= 0 && index < m_names.size();
 	}
 
 	typedef std::exception Exception;
