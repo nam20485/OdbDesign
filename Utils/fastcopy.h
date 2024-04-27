0e0b0b94bb0828460d2dab39b1ef9e3b4c396d3a
Nathan Miller <nmiller217@gmail.com>
2024-04-27 14:14:48 -0700

    remove commented code

--- Utils/fastcopy.h
+++ Utils/fastcopy.h
@@ -1,14 +1,11 @@
 #pragma once
 
 #include <filesystem>
-#include <string>
 #include "utils_export.h"
+#include <system_error>
 
 namespace Utils
 {
 	UTILS_EXPORT std::error_code copy(const std::filesystem::path& source, const std::filesystem::path& dest, bool overwriteExisting);
-	//UTILS_EXPORT std::error_code copy(const std::string& source, const std::string& dest, bool overwriteExisting);
-
-	//UTILS_EXPORT std::error_code fastcopy(const std::string& source, const std::string& dest, bool overwriteExisting);
 	UTILS_EXPORT std::error_code fastcopy(const std::filesystem::path& source, const std::filesystem::path& dest, bool overwriteExisting);
 }
