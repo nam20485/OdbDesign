a3cdf0b2abc38aed9f823af0d3e02fbaf77c0d0e
Nathan Miller <nmiller217@gmail.com>
2024-04-27 08:39:59 -0700

    minor refactor

--- Utils/EnumMap.h
+++ Utils/EnumMap.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <exception>
+#include "str_utils.h"
 
 namespace Utils
 {
@@ -10,47 +11,71 @@ namespace Utils
 	class EnumMap
 	{
 	public:
-		EnumMap(const std::vector<std::string>& names)
-			: m_names(names)
-		{}
+		EnumMap(const std::vector<std::string>& names, bool caseInsensitive = false);
 
-		const std::string& getValue(const E e) const
-		{
-			auto index = static_cast<size_t>(e);
-			if (index < 0 || index >= m_names.size())
-			{
-				std::string msg = "no name found for value: " + std::to_string(index);
-				throw Exception(msg.c_str());
-			}
-			return m_names[index];
-		}
+		const std::string& getValue(const E e) const;
+		E getValue(const std::string& name) const;
 
-		E getValue(const std::string& name) const
-		{
-			auto findIt = std::find(m_names.begin(), m_names.end(), name);			
-			if (findIt == m_names.end())
-			{
-				std::string msg = "no value found for name: (" + name + ")";
-				throw Exception(msg.c_str());
-			}
-			return static_cast<E>(std::distance(m_names.begin(), findIt));
-		}
-
-		bool contains(const std::string& name) const
-		{
-			return std::find(m_names.begin(), m_names.end(), name) != m_names.end();
-		}
-
-		bool contains(const E e) const
-		{
-			auto index = static_cast<size_t>(e);
-			return index >= 0 && index < m_names.size();
-		}
+		bool contains(const std::string& name) const;
+		bool contains(const E e) const;
 
 		typedef std::exception Exception;
 
 	private:
 		const std::vector<std::string> m_names;
-
+		const bool m_bCaseInsensitive;		
+		
 	};
+
+	template<typename E>
+	inline EnumMap<E>::EnumMap(const std::vector<std::string>& names, bool caseInsensitive)
+		: m_names(names)
+		, m_bCaseInsensitive(caseInsensitive)
+	{}
+
+	template<typename E>
+	inline const std::string& EnumMap<E>::getValue(const E e) const
+	{
+		auto index = static_cast<size_t>(e);
+		if (index < 0 || index >= m_names.size())
+		{
+			std::string msg = "no name found for value: " + std::to_string(index);
+			throw Exception(msg.c_str());
+		}
+		return m_names[index];
+	}
+
+	template<typename E>
+	inline E EnumMap<E>::getValue(const std::string& name) const
+	{
+		std::vector<std::string>::const_iterator findIt;
+		if (m_bCaseInsensitive)
+		{
+			findIt = Utils::find_str_icmp(m_names.begin(), m_names.end(), name);
+		}
+		else
+		{
+			findIt = std::find(m_names.begin(), m_names.end(), name);
+		}
+
+		if (findIt == m_names.end())
+		{
+			std::string msg = "no value found for name: (" + name + ")";
+			throw Exception(msg.c_str());
+		}
+		return static_cast<E>(std::distance(m_names.begin(), findIt));
+	}
+
+	template<typename E>
+	inline bool EnumMap<E>::contains(const std::string& name) const
+	{
+		return std::find(m_names.begin(), m_names.end(), name) != m_names.end();
+	}
+
+	template<typename E>
+	inline bool EnumMap<E>::contains(const E e) const
+	{
+		auto index = static_cast<size_t>(e);
+		return index >= 0 && index < m_names.size();
+	}
 }
