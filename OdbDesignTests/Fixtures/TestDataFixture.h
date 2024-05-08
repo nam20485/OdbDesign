6a92ce7de39a7009d7a487d24f2ac33bb598092e
Nathan Miller <nmiller217@gmail.com>
2024-05-08 12:21:02 -0700

    move constant

--- OdbDesignTests/Fixtures/TestDataFixture.h
+++ OdbDesignTests/Fixtures/TestDataFixture.h
@@ -19,11 +19,11 @@ namespace Odb::Test::Fixtures
 		std::filesystem::path getTestDataFilesDir() const;
 		std::filesystem::path getTestDataFilePath(const std::string& filename) const;
 
-		static inline constexpr const char ODB_TEST_DATA_DIR_ENV_NAME[] = "ODB_TEST_DATA_DIR";
-		static inline constexpr const char TESTDATA_FILES_DIR[] = "FILES";
+		static inline constexpr const char ODB_TEST_DATA_DIR_ENV_NAME[] = "ODB_TEST_DATA_DIR";		
 		static inline constexpr const char ODB_TEST_ENV_NAME[] = "ODB_TEST_ENVIRONMENT_VARIABLE";
 		static inline constexpr const char ODB_TEST_ENV_VALUE[] = "ODB_TEST_ENVIRONMENT_VARIABLE EXISTS";
 		static inline constexpr const char ODB_TEST_NONEXISTENT_ENV_NAME[] = "THIS_ENVIRONEMNT_VARIABLE_DOES_NOT_EXIST";
+		static inline constexpr const char TESTDATA_FILES_DIR[] = "FILES";
 		
 
 		static inline constexpr bool ENABLE_TEST_LOGGING = false;
