5c332272bdac9a9ff103d10ee6abe23e3fd7617d
Nathan Miller <nmiller217@gmail.com>
2025-06-13 22:55:01 -0700

    add -y -qq to apt call to run non-interaactive mode in script

--- scripts/install-dependencies-deb.sh
+++ scripts/install-dependencies-deb.sh
@@ -2,7 +2,8 @@
 set -e
 
 sudo apt update
-sudo apt install    git \
+sudo apt install -y -qq --no-install-recommends \
+                    git \
                     cmake \
                     ninja-build \
                     build-essential \
